4f381e849252f7b976884952e93e843e6c2f89eb
lmcmz <lmcmze@gmail.com>
2024-07-31 21:45:37 +1000

    Update key lookup host

--- pages/api/getAddressByIndexer.js
+++ pages/api/getAddressByIndexer.js
@@ -1,7 +1,7 @@
 export default async function getAddressByIndexer(req, res) {
     const { publicKey } = req.query;
     console.log("publicKey ==>", publicKey)
-    const url = `https://key-indexer.production.flow.com/key/${publicKey}`;
+    const url = `https://production.key-indexer.flow.com/key/${publicKey}`;
     console.log("url ==>", url)
     const result = await fetch(url);
     const json = await result.json();
